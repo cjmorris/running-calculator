e089e91a9382bafd74374b759651400b109efd08
Cameron Morris <cameronjmorris87@gmail.com>
2023-03-16 19:35:32 +1300

    Updated js link to use defer

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-if(document.readyState == 'loading'){
-    document.addEventListener('DOMContentLoaded', ready)
-} else{
-    ready()
-}
+var siteLogo = document.getElementsByClassName('site-logo')[0]
+siteLogo.addEventListener('click', navigateHome)
 
-function ready(){
-    var siteLogo = document.getElementsByClassName('site-logo')[0]
-    siteLogo.addEventListener('click', navigateHome)
-}
 
 function navigateHome(){
     location.href = 'index.html';
